91521b36adc69fa674334533ee7cbd992ca0c2b1
Lukas Bombach <lukas.bombach@googlemail.com>
2023-09-03 11:10:43 +0200

    nextjs + colored textarea

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="container h-screen p-24">
+      <textarea
+        className="w-full h-full outline-none caret-black font-mono bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+        value=" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit labore, iste voluptate repellendus laudantium
+        omnis modi culpa quod quibusdam nesciunt corrupti! Dolor quaerat in, a ipsam et itaque cumque dignissimos."
+      />
+    </main>
+  );
+}
