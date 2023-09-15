55a79f6bf809d54fb295d6dc4c95c7718e88b439
Lukas Bombach <lukas.bombach@googlemail.com>
2023-09-15 07:12:42 +0200

    something is visible

--- src/components/EditorShiki.tsx
+++ src/components/EditorShiki.tsx
@@ -1,30 +1,48 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { getHighlighter, setCDN } from "shiki";
-
-import type { IThemedToken } from "shiki";
+import { encode } from "bmp-js";
 
 setCDN("/_next/static/shiki/");
 
-const FALLBACK_COLOR = "#FF00FF";
+const FALLBACK_COLOR = "#FFFFFF";
 
-const drawCanvas = (canvas: HTMLCanvasElement, linesOfTokens: IThemedToken[][]) => {
-  const ctx = canvas.getContext("2d")!;
-  const height = linesOfTokens.length;
-  const width = linesOfTokens.reduce((max, line) => Math.max(max, line.length), 0);
+function createBMP(array: string[][]): string {
+  const height = array.length;
+  const width = Math.max(...array.map(row => row.length));
 
-  canvas.height = height;
-  canvas.width = width;
+  const buffer = Buffer.alloc(width * height * 4);
 
+  let offset = 0;
   for (let y = 0; y < height; y++) {
-    for (let x = 0; x < linesOfTokens[y].length; x++) {
-      const token = linesOfTokens[y][x];
-      const color = token.color || FALLBACK_COLOR;
-      ctx.fillStyle = color;
-      ctx.fillRect(x, y, 1, 1);
+    for (let x = 0; x < width; x++) {
+      const color = array[y][x] || FALLBACK_COLOR;
+      const r = parseInt(color.substring(1, 3), 16);
+      const g = parseInt(color.substring(3, 5), 16);
+      const b = parseInt(color.substring(5, 7), 16);
+      const a = 255;
+
+      console.log(color, r, g, b);
+
+      buffer[offset] = r;
+      buffer[offset + 1] = b;
+      buffer[offset + 2] = g;
+      buffer[offset + 3] = r;
+
+      offset += 4;
     }
   }
-};
+
+  const bmpData = {
+    data: buffer,
+    width: width,
+    height: height,
+  };
+
+  const rawData = encode(bmpData);
+  return rawData.data.toString("base64");
+}
 
 const sourceCode = `
 const x = 10;
@@ -33,17 +51,38 @@ function hello() {
   console.log("Hello, world!", x);
 }`.trim();
 
-getHighlighter({
-  theme: "nord",
-}).then(highlighter => {
-  console.log(JSON.stringify(highlighter.codeToThemedTokens(sourceCode, "js"), null, 2));
-});
-
 export function Editor() {
+  // ref to the textarea
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  // highlight the source code
+  useEffect(() => {
+    getHighlighter({
+      theme: "nord",
+    }).then(highlighter => {
+      const colors = highlighter
+        .codeToThemedTokens(sourceCode, "js")
+        .map(line =>
+          line.flatMap(token => new Array(token.content.length).fill((token.color || FALLBACK_COLOR).substring(0, 7)))
+        );
+      const height = colors.length;
+      const width = Math.max(...colors.map(row => row.length));
+      const base64 = createBMP(colors);
+      console.log(colors);
+      console.log(base64);
+      ref.current!.style.backgroundImage = `url(data:image/bmp;base64,${base64})`;
+      ref.current!.style.backgroundSize = `${width}ch ${height * 1.5}em`;
+      ref.current!.style.backgroundRepeat = `no-repeat`;
+      ref.current!.style.imageRendering = `pixelated`;
+    });
+  }, []);
+
   return (
     <textarea
-      className="w-full h-full outline-none caret-black font-mono bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+      className="w-full h-full outline-none caret-black font-mono "
+      // bg-clip-text text-transparent
       defaultValue={sourceCode}
+      ref={ref}
     />
   );
 }
