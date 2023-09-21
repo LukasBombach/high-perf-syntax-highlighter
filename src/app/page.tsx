79ab6cec6511d605e46e2a19a420b3af739d6b5f
Lukas Bombach <lukas.bombach@googlemail.com>
2023-09-21 09:31:01 +0200

    rm -rf *

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Editor } from "../components/EditorShiki";
-
-export default function Home() {
-  return (
-    <main className="container h-screen p-24 bg-[#282c34]">
-      <Editor />
-    </main>
-  );
-}
