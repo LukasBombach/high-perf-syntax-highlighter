28684a7df921418bcddce47a197dafd5a7be4eed
Lukas Bombach <lukas.bombach@googlemail.com>
2023-09-22 14:16:38 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import "./style.css";
 
-const app = document.getElementById("app");
-
-if (!app) {
-  throw new Error("Failed to find app element");
-}
-
 const textarea = document.createElement("textarea");
-app.classList.add("w-full", "h-full", "bg-gray-900", "text-white");
-app.appendChild(textarea);
+textarea.classList.add("container", "mx-auto", "px-4", "h-96", "bg-zinc-800", "text-zinc-50");
+document.body.append(textarea);
